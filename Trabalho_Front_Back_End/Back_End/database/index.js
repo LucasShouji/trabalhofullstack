e5085be91c498468ab08b70c430771adb7a7b46f
LucasShouji <140670816+LucasShouji@users.noreply.github.com>
2024-09-08 23:34:24 -0300

    a

--- Trabalho_Front_Back_End/Back_End/database/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const config = require('../config/database')
-
-const Cadastro = require('../models/Cadastro')
-
-const connection = new Sequelize(config)
-
-module.exports = connection;
