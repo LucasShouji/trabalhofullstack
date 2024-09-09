8af9bdbdefce6c475f77fb419c9f94c290a11930
LucasShouji <140670816+LucasShouji@users.noreply.github.com>
2024-09-09 15:31:44 -0300

    Update 20240906014848-create-endereco.js

--- Back_End/migrations/20240906014848-create-endereco.js
+++ Back_End/migrations/20240906014848-create-endereco.js
@@ -4,7 +4,7 @@ module.exports = {
  up: async(queryInterface, Sequelize) => {
    await queryInterface.createTable('cadastros', {
     cpf: {
-      type: Sequelize.LONG,
+      type: Sequelize.STRING,
       primaryKey: true,
       autoIncrement: true,
       allowNull: false,
